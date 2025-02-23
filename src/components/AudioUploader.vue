1911c81b7eae9ad6a061166516c0ed330c28302a
Averyden <igo408gt@gmail.com>
2025-02-23 13:05:08 +0100

    Set up handling

--- src/components/AudioUploader.vue
+++ src/components/AudioUploader.vue
@@ -12,7 +12,15 @@ export default defineComponent({
   emits: ['file-upload'],
 
   methods: {
-    handleFileUpload(event: Event) {},
+    handleFileUpload(event: Event) {
+      const target = event.target as HTMLInputElement
+      const file = target.files?.[0]
+      if (!file) return
+
+      if (!file.type.startsWith('audio/')) {
+        //TODO: make some sort of error message here.
+      }
+    },
   },
 })
 </script>

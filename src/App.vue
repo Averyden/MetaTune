2a5fbe9b3da15dabf912ac270867944c4dc98a64
Averyden <igo408gt@gmail.com>
2025-02-23 13:13:06 +0100

    Set up base structure for app.vue

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>MetaTune</h1>
+    <AudioUploader @file-uploaded="handleFile" />
+    <p v-if="uploadedFile">Uploaded: {{ uploadedFile.name }}</p>
+  </div>
+</template>
+
+<script lang="ts">
+import {defineComponent, ref} from "vue";
+import AudioUploader from "@/components/AudioUploader.vue"
+
+export default defineComponent({
+  name: "App",
+  components: {
+    AudioUploader
+  },
+
+})

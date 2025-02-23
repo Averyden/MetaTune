d5264b1af0c09990c3ec2b883ea81608d91294b8
Averyden <igo408gt@gmail.com>
2025-02-23 18:27:05 +0100

    Script the thingy uh

--- src/components/AudioPlayer.vue
+++ src/components/AudioPlayer.vue
@@ -3,3 +3,23 @@
     <audio controls :src="src"></audio>
   </div>
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'AudioPlayer',
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style>
+.audioPlayer {
+  margin-top: 20px;
+}
+</style>

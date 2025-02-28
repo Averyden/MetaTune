13dee514b2666fa2afc2c91f2829a759ff925532
Averyden <igo408gt@gmail.com>
2025-02-28 10:48:45 +0100

    Define the base component

--- src/components/MetaDataViewer.vue
+++ src/components/MetaDataViewer.vue
@@ -3,5 +3,10 @@
 </template>
 
 <script lang="ts">
+import { defineComponent } from 'vue';
 
+export default defineComponent({
+    name: 'MetaDataViewer',
+  // TODO: Just slowly implement whatever over time  
+})
 </script>

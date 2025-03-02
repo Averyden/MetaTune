9cb449494e2e03412d83365ab0e25ad15e0a32fc
Averyden <igo408gt@gmail.com>
2025-03-02 22:18:53 +0100

    Set up the basic order of basic data

--- src/components/MetaDataViewer.vue
+++ src/components/MetaDataViewer.vue
@@ -1,7 +1,11 @@
 <template>
   <div id="metadataViewer" v-if="metadata">
     <h2>Metadata</h2>
-    <p><strong>Title:</strong> {{}}</p>
+    <p><strong>Title:</strong> {{ metadata.title }}</p>
+    <p><strong>Artist:</strong> {{ metadata.artist }}</p>
+    <p><strong>Album:</strong> {{ metadata.album }}</p>
+    <p><strong>Track:</strong> {{ metadata.track?.no }}</p>
+
     <img src="../assets/placeholder.png" />
   </div>
 </template>

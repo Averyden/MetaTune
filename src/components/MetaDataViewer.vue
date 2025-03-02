3addbab3b15276265c73241df0ff821f07ccd033
Averyden <igo408gt@gmail.com>
2025-03-02 22:16:12 +0100

    Create a prop for metadata

--- src/components/MetaDataViewer.vue
+++ src/components/MetaDataViewer.vue
@@ -1,12 +1,19 @@
 <template>
-<img src="../assets/placeholder.png">
+  <div id="metadataViewer" v-if="metadata">
+    <h2>Metadata</h2>
+    <p><strong>Title:</strong> {{}}</p>
+    <img src="../assets/placeholder.png" />
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent } from 'vue'
 
 export default defineComponent({
-    name: 'MetaDataViewer',
-  // TODO: Just slowly implement whatever over time  
+  name: 'MetaDataViewer',
+  props: {
+    metadata: Object,
+  },
+  // TODO: Just slowly implement whatever over time
 })
 </script>

87c2988f4e3f9d0e08f47b38250b919835f76aa5
Averyden <igo408gt@gmail.com>
2025-03-09 17:32:15 +0100

    Set up the basic design of the componentn

--- src/components/MetaDataEditor.vue
+++ src/components/MetaDataEditor.vue
@@ -1,3 +1,21 @@
-<template></template>
+<template>
+  <div class="modal">
+    <div class="modalContent">
+      <h2>Editing Metadata</h2>
+      <label>Title:</label>
+      <!--TODO: bind the v-model types properly thanks-->
+      <input v-model="title" type="text" />
+
+      <label>Artist:</label>
+      <input v-model="artist" type="text" />
+
+      <label>Album:</label>
+      <input v-model="album" type="text" />
+
+      <button>Save</button>
+      <button>Cancel</button>
+    </div>
+  </div>
+</template>
 
 <script lang="ts"></script>

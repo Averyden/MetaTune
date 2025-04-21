c842b4a1e88526f39e258863f3ffb659cd7b85f5
Averyden <igo408gt@gmail.com>
2025-04-21 13:00:47 +0200

    Implement method for toggling dark mode

---
+++ src/components/DarkModeToggle.vue
@@ -0,0 +1,21 @@
+<template>
+  <button class="darkModeToggle" @click="toggleDarkMode">
+    <img
+      :src="isDark ? 'src/assets/icons/sun.png' : 'src/assets/icons/moon.png'"
+      alt="Toggle Dark Mode"
+      class="icon"
+    />
+  </button>
+</template>
+
+<script lang="ts">
+import { ref } from 'vue'
+
+const isDark = ref(false)
+
+const toggleDarkMode = () => {
+  isDark.value = !isDark.value
+  document.documentElement.classList.toggle('darkMode', isDark.value)
+  localStorage.setItem('darkMode', isDark.value.toString())
+}
+</script>
